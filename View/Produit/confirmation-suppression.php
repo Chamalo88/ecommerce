a40bc8caa7f9f3414c1b2d6942aa7771907b5306
Chamalo88 <80330845+Chamalo88@users.noreply.github.com>
2021-10-11 14:31:25 -0700

    demarrage2

--- View/Produit/confirmation-suppression.php
+++
@@ -1,9 +0,0 @@
-<div >
-    <H1> ETES VOUS SUR DE VOULOIR SUPPRIMER CE PRODUIT ?</h1>
-    <form method="POST">
-        <input name="confirmation" type="submit" value="Supprimer le produit" class="btn btn-danger">
-    </form>
-
-    <a style="margin-left: 20px;" href="<?= Config::$baseUrl ?>/produit/afficherTout" class="btn btn-info">Annuler</a>
-
-</div>
